83d7d8b25ab9155c74f050d13867a771bb930932
SinoCaga <Oyisacaga@gmail.com>
2022-06-12 19:01:15 +0200

    Add project files.

---
+++ Data/JokesOnYouContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JokesOnYou.Models;
+
+namespace JokesOnYou.Data
+{
+    public class JokesOnYouContext : DbContext
+    {
+        public JokesOnYouContext (DbContextOptions<JokesOnYouContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JokesOnYou.Models.Joke>? Joke { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using JokesOnYou.Data;
+
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<JokesOnYouContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("JokesOnYouContext") ?? throw new InvalidOperationException("Connection string 'JokesOnYouContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
